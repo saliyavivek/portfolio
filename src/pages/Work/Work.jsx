57e7981651b678d60f271831252f8b9261830ca9
Vivek Saliya <viveksaliya004@gmail.com>
2024-06-06 21:29:39 +0530

    small progress on work page

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Work.css";
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="work">
+      <div className="work__title">
+        <h3>work</h3>
+        <p>a brief summary of my work so far</p>
+      </div>
+      <div className="work__w">
+        <div className="work_w_top">
+          <h4>bluelearn • 2021 - present</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
