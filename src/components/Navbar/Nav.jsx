ec067368d797bcec9690a5f910b4ac2497eac49c
Vivek Saliya <viveksaliya004@gmail.com>
2024-06-23 12:32:32 +0530

    fix: home page

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -151,7 +151,7 @@ const Nav = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
                 role="img"
-                class="social-icon iconify iconify--ph"
+                className="social-icon iconify iconify--ph"
                 width="1em"
                 height="1em"
                 viewBox="0 0 256 256"
