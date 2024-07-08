65f7ee2c6157753b75ec9807994e4e2da7788b05
Vivek Saliya <viveksaliya004@gmail.com>
2024-07-08 21:56:20 +0530

    add skills' links

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -17,22 +17,36 @@ const Skills = () => {
           technologies that allows me to build robust, scalable, and efficient
           web applications.{" "}
           <p>
-            With a solid foundation in <span>MongoDB</span>, I adeptly manage
-            databases, ensuring data is organized and easily retrievable.
+            With a solid foundation in{" "}
+            <a href="https://www.mongodb.com/" target="_blank">
+              MongoDB
+            </a>
+            , I adeptly manage databases, ensuring data is organized and easily
+            retrievable.
           </p>
           <p>
-            My expertise in <span>Express.js</span> enables me to create
-            seamless server-side logic, ensuring that applications run smoothly
-            and efficiently.{" "}
+            My expertise in{" "}
+            <a href="https://expressjs.com/" target="_blank">
+              Express.js
+            </a>{" "}
+            enables me to create seamless server-side logic, ensuring that
+            applications run smoothly and efficiently.{" "}
           </p>
           <p>
-            <span>React.js</span> is where my front-end skills truly shine; I
-            build dynamic and responsive user interfaces that provide an
-            exceptional user experience.
+            <a href="https://react.dev/" target="_blank">
+              React.js
+            </a>{" "}
+            is where my front-end skills truly shine; I build dynamic and
+            responsive user interfaces that provide an exceptional user
+            experience.
           </p>{" "}
           <p>
-            Finally, <span>Node.js</span> ties everything together, allowing me
-            to develop fast and scalable server-side applications.
+            Finally,{" "}
+            <a href="https://nodejs.org/en" target="_blank">
+              Node.js
+            </a>{" "}
+            ties everything together, allowing me to develop fast and scalable
+            server-side applications.
           </p>
         </p>
         <p>
@@ -42,8 +56,11 @@ const Skills = () => {
           </p>
           <p>
             My experience with<span> version control systems</span> like{" "}
-            <span>Git</span> ensures that I can collaborate effectively with
-            teams and maintain clean and organized codebases.
+            <a href="https://git-scm.com/" target="_blank">
+              Git
+            </a>{" "}
+            ensures that I can collaborate effectively with teams and maintain
+            clean and organized codebases.
           </p>
         </p>
       </div>
