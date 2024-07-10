6786be1cd8d0c329e09db5196c35f63f539648f0
Vivek Saliya <viveksaliya004@gmail.com>
2024-07-10 20:52:47 +0530

    remove like feat

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -30,33 +30,6 @@ const Home = () => {
             </a>
             .
           </p>
-          <p className="para-likes">
-            Enjoying your time here? Show some love with a like!
-            {/* Drop a like to let me know you're vibing in here! */}
-            {/* Enjoying your visit? Show your support by liking this page{" "} */}
-            <button
-              onClick={() => {
-                handleLike();
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="img"
-                className="social-icon iconify iconify--ph"
-                width="1em"
-                height="1em"
-                viewBox="0 0 24 24"
-                fill={liked ? "#6F736D" : "none"}
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
-              </svg>
-            </button>
-          </p>
         </div>
       </div>
     </div>
